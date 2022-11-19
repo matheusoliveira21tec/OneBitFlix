3dec423339599182a6e5f7d2d0f65f01ba8d6b71
monegasco <mhoa.2000@gmail.com>
2022-11-19 19:30:49 -0300

    configurando o sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-// src/server.ts
-
 import express from 'express'
+import { database } from './database'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server started successfuly at port ${PORT}`)
+    database.authenticate().then(() => {
+        console.log('DB connection successfull.')
+    })
+    console.log(`Server started successfuly at port ${PORT}`)
 })
