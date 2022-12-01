c36e7d5388bee80754aa3a81e38dc4687795d2ce
monegasco <mhoa.2000@gmail.com>
2022-11-30 23:27:37 -0300

    adicionando rota index categoria

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { database } from './database'
+import { router } from './routes'
 import { adminJs, adminJsRouter } from './adminJs'
 
 const app = express()
 
 app.use(express.static('public'))
+
+app.use(router)
 
 app.use(adminJs.options.rootPath, adminJsRouter)
 
